75c5e9494f44a607ae1bc6b0511e104d9e77e520
ibrainpop <ibrainpop@gmail.com>
2019-05-03 14:46:45 +0200

    new connex html et main js

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
     $("#rejoindre").on("click", Rejoindre);
 })
-function Rejoindre(){
+function Rejoindre(e){
+    e.preventDefault();
     var  name = $("#joinChat").val();
     $.ajax({
         url : 'rejoindre.php',
         method : 'post',
-        data : {name :name},
+        data : {Name : name},
         dataType : 'json',
         success : function(data){
-            console.log(data);
+            if(data.result == true){ //redirection vers page de connexion
+                window.location.replace("connexion.html");
+            }else{
+                $("#error-msg").append('<div class="alert alert-danger" role="alert">Cette discussion existe déjà</div>');
+            }
         }
 
     })
