6e957643eb5e9b6aceb8ad999043b9d2df14a753
sabineleydier <leydier.sabine@gmail.com>
2019-05-06 15:30:01 +0200

    coucou

--- main2.js
+++ main2.js
@@ -1,58 +1,72 @@
+var pseudo;
+var idName;
+
 $(document).ready(function(){
-    idNumber();
-    processUser();
+    
+     pseudo = processUser();  
+     idName = idNumber();
+     afficheMessage();
     $("#sendMessage").on("click", sendMessage);
 })
 
 //fonction permettant de récuperer le nom de la discussion en fonction de l'id
-function idNumber(){
+function idNumber(id){
     var parameters = location.search.substring(1).split("&");
     var temp = parameters[0].split("=");
     var id = unescape(temp[1]);
-    console.log(id);
+    
     $.ajax({
         url : 'infosReturn.php',
         method : 'post',
         data :  {Id_Discussion : id},
         dataType : 'json',
-        success : function(nameId){
-            console.log(nameId);
-            return nameId;
+        success : function(nameId){            
+            $(".discussion").append(nameId.Name);
+            
         }
     })
+    return id;
 }
 
-//fonction permettant de récuperer les paramètres dans l'url
-function processUser()
-  {
-    
+//fonction permettant de récuperer le paramètre pseudo dans l'url
+function processUser(){    
     var parameters = location.search.substring(1).split("&");
-
-    // var temp = parameters[0].split("=");
-    // var id = unescape(temp[1]);
    var temp = parameters[1].split("=");
     var pseudo = unescape(temp[1]);
-    console.log(pseudo);
+    $(".utulisateur").append(pseudo);   
     return pseudo;
-
-    // $(".discussion").append(id);
-    // $(".utulisateur").append(pseudo);
-   
+  
   }
+  
 
   //fonction permettant d'envoyer les messages
-  function sendMessage(){
-      var user = processUder();
-      var idName = idNumber();
+  function sendMessage(){       
       var message = $("writtenMessage").val();
+    
       $.ajax({
           url : 'chat.php',
           method : 'post',
-          data : {Content : message, Id_Discussion : idName, User_Peudo : user },
-          dataType : 'json',
-          success : function(donnee){
-              console.log(donnee);
+          data : {Content : message, Id_Discussion : idName, User_Peudo : pseudo},
+          success : function(){
+              console.log("message envoyé");
           }
+          
 
+      })
+  }
+
+  //fonction permettant de renvoyer les messages du chat
+  function afficheMessage(){
+      $.ajax({
+          url : 'chatDisplay.php',
+          method : 'post',
+          dataType : 'json',
+          success : function (data){
+            for(var i = 0; i<data.length; i++){
+                console.log(data[i].Content);
+                $("#displayChat1").text(data[i].Content);
+                $("#displayChat1").after('<p>Envoyé par ' + data[i].User_Pseudo + " le " + data[i].DateTime);
+            }
+          }
       })
   }
