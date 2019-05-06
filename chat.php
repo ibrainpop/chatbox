14ba070939f52b4ce3ccaf1548c37fb57382eadd
ibrainpop <ibrainpop@gmail.com>
2019-05-06 13:51:27 +0200

    chat.php meme pas modif

--- chat.php
+++ chat.php
@@ -9,11 +9,7 @@ $id = $_POST["Id_Discussion"];
 if(!empty($_POST["User_Pseudo"])){
 
     $requete = $bdd->prepare("
-<<<<<<< HEAD
-    INSERT INTO message Id_Discussion, User_Pseudo, Content, DateTime VALUES( ?, ?, ?, ?, ?)
-=======
     INSERT INTO message Id_message, Id_Discussion, User_Pseudo, Content (?, ?, ?, ?)
->>>>>>> c5c2f5918daa606cf258742b7660fbbb9f4de3b8
     ");
 
     $requete->execute([$newPseudo]);
