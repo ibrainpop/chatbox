3e69f8946f4f5b4ae2c22702f2313aea3cf4f95f
ibrainpop <ibrainpop@gmail.com>
2019-05-06 14:04:25 +0200

    modif infos return

--- infosReturn.php
+++ infosReturn.php
@@ -5,10 +5,10 @@ include "bdd_connection.php";
 $discussionName = $_POST["Name"];
 
 $requete = $bdd->prepare("
-    SELECT Name FROM discussion WHERE Id_Discussion
+    SELECT Name FROM discussion WHERE Id_Discussion = ?
     ");
 
 $requete->execute([$discussionName]);
 $returnedName = $requete->fetch();
 
-echo json_encode($discussionName);
+echo json_encode($returnedName);
